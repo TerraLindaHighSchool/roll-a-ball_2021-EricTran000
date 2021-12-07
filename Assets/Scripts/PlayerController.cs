82a9b6600f45c5e1eeb7421b5068a3d44b052f5a
EricTran000 <89489669+EricTran000@users.noreply.github.com>
2021-12-07 17:41:29 -0600

    Finished a level and an on ground detector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class PlayerController : MonoBehaviour
@@ -9,7 +10,9 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
+    [SerializeField] private float jumpForce = 500;
 
+    private bool isOnGround;
     private Rigidbody rb;
     private int count;
     private float movementX;
@@ -45,7 +48,9 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
         rb.AddForce(movement * speed);
+        Restart();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -58,4 +63,47 @@ public class PlayerController : MonoBehaviour
             SetCountText();
         }
     }
+   
+    void OnJump()
+    {
+        if (isOnGround)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0));
+        }
+
+
+    }
+
+    void Restart()
+    {
+        if(transform.position.y < -10)
+        {
+            SceneManager.LoadScene("h");
+        }
+    }
+
+    void OnLose()
+    {
+        SceneManager.LoadScene("h");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.name == "Ground")
+        {
+            isOnGround = true;
+        }
+
+   
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Ground")
+        {
+            isOnGround = false;
+        }
+    }
 }
+
